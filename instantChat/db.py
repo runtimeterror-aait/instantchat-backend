761a38f1d5b5ed711b7888edf5dd3b58e0501e36
michael belete <it.michael.belete@gmail.com>
2021-05-30 16:19:52 +0300

    created db for monogodb

--- instantChat/db.py
+++ instantChat/db.py
@@ -1,12 +1,13 @@
 from flask import g
+from flask_mongoengine import MongoEngine
 from mongoengine import connect
 from mongoengine.connection import disconnect
 
 def get_db():
-    db = connect('instantchat', host='127.0.0.1', port=27017) 
-    g.db = db
-    # connect('my_db', username='my_user', password='my_password', authentication_source='admin')
-
+    if 'db' not in g:
+        g.db = MongoEngine()
+    return g.db
+    
 def close_db(e=None):
     
     db = g.pop("db", None)
@@ -14,3 +15,10 @@ def close_db(e=None):
     if db is not None:
         disconnect(alias="instantchat")
 
+def init_app(app):
+    app.config['MONGODB_SETTINGS'] = {
+        'db': 'instantchat',
+        'host': '127.0.0.1',
+        'port': 27017
+    }
+    get_db().init_app(app)
