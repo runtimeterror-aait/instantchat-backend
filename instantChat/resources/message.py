c01281394e10ffdfd4cec67321e8d7742c0277ea
Naol Dame <naododa5566@gmail.com>
2021-06-04 01:41:18 +0300

    Put method for messae.py

--- instantChat/resources/message.py
+++ instantChat/resources/message.py
@@ -6,9 +6,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from instantChat.models.message import Message as Usermessage,TextMessage
 
 # project resources
-from models.message import Messages
-# from models.users import Users
-# from api.errors import forbidden
+from api.errors import forbidden
 
 class messages(Resource):
     @jwt_required
@@ -19,7 +17,7 @@ class messages(Resource):
         
 
     @jwt_required
-    def post(self)-> Response:
+    def post(self) -> Response:
         data = request.get_json()
         user_message = Usermessage.objects.get(id=get_jwt_identity())
         textMessage = TextMessage(**data)
@@ -28,4 +26,18 @@ class messages(Resource):
         return jsonify({'data': user_message.TextMessage})
 
 
- 
+class messages(Resource):
+    @jwt_required
+    def get(self, message_id: str) -> Response:
+        authUser = Usermessage.objects.get(id=get_jwt_identity())
+        TextMessage = authUser.TextMessage
+        return jsonify({'data':TextMessage})
+
+
+    @jwt_required
+    def put(self, message_id: str)->Response:
+        data = request.get_json()
+        user_message = Usermessage.objects.get(id=get_jwt_identity())
+        textmessage = TextMessage.objects(id=message_id)
+        textmessage.update(**data)
+        return jsonify({'data': user_message.textmessage})
