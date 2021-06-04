660059d98cdc44503e967332d114cbd2131f03cc
michael belete <it.michael.belete@gmail.com>
2021-06-04 18:03:38 +0300

    created search by phone

--- instantChat/resources/user.py
+++ instantChat/resources/user.py
@@ -6,48 +6,34 @@ from instantChat.models.user import User as UserModel
 # project resources
 from instantChat.api.error import forbidden
 
+
 class UserResource(Resource):
     @jwt_required()
     def get(self, user_id: str) -> Response:
         if user_id == "self":
             output = UserModel.objects.get(id=get_jwt_identity())
         else:
-            output = UserModel.objects.get(id=user_id)
+            output = UserModel.objects.get(phone=user_id)
+            if output.count() == 0:
+                output = UserModel.objects.get(id=user_id)
+                return jsonify({'data': output})
         return jsonify({'data': output})
-        
 
     @jwt_required()
     def put(self, user_id: str) -> Response:
-
-        authorized: bool = UserModel.objects.get(id=get_jwt_identity())
-
-        if authorized:
-            data = request.get_json()
-            put_user = UserModel.objects(id=user_id).update(**data)
-            output = {'id': str(put_user.id)}
-            return jsonify({'result': output})
-        else:
-            return forbidden()
+        data = request.get_json()
+        put_user = UserModel.objects(id=user_id).update(**data)
+        output = {'id': str(put_user.id)}
+        return jsonify({'result': output})
 
     @jwt_required()
     def post(self) -> Response:
-        authorized: bool = UserModel.objects.get(id=get_jwt_identity())
-
-        if authorized:
-            data = request.get_json()
-            post_user = UserModel(**data).save()
-            output = {'id': str(post_user.id)}
-            return jsonify({'result': output})
-        else:
-            return forbidden()
+        data = request.get_json()
+        post_user = UserModel(**data).save()
+        output = {'id': str(post_user.id)}
+        return jsonify({'result': output})
 
     @jwt_required()
     def delete(self, user_id: str) -> Response:
-        authorized: bool = UserModel.objects.get(id=get_jwt_identity())
-
-        if authorized:
-            output = UserModel.objects(id=user_id).delete()
-            return jsonify({'result': output})
-        else:
-            return forbidden()
-
+        output = UserModel.objects(id=user_id).delete()
+        return jsonify({'result': output})
