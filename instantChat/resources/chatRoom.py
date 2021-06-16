7e06e82e2ba3aa945ea5351fc3e6d1717da29c52
BgitBB <61750923+BgitBB@users.noreply.github.com>
2021-06-16 20:58:11 +0300

    changed get api/ popular chatrooms
    might wanna improve by using mongoengine builtin methods

--- instantChat/resources/chatRoom.py
+++ instantChat/resources/chatRoom.py
@@ -53,7 +53,7 @@ class ChatRoom(Resource):
 
         for member in newMembersList:
             if ChatRoomModel.objects.get(id=chat_room_id, members=member).count() == 0:
-                chatRoom.objects.get(id=chat_room_id).members.append(member)
+                ChatRoom.objects.get(id=chat_room_id).members.append(member)
 
         return jsonify({"message": "New members has been added"})
 
@@ -83,23 +83,23 @@ class ChatRoom(Resource):
 class PopularChatRoom(Resource):
     @jwt_required()
     def get(self) -> Response:
-        chatRoom = ChatRoomModel.objects.order_by("members").members[:6]
+        # chatRoom = ChatRoomModel.objects.order_by("members").members[:6]
         # chatRoom = ChatRoomModel.objects.order_by("-members.length")[:6]
 
-        #check and decide #mk #tb
-        # max = [-1,-1,-1,-1,-1,-1]
-        # room_ids = [-1,-1,-1,-1,-1,-1]
-        # for room in ChatRoomModel.objects.get_or_404(): #(privateMessaging=False) might help
-        #     for i in range(max.length):
-        #         if room.members.length > max[i]:
-        #             max = max[:i] + [room.members.length] + max[i:5] #shift right
-        #             room_ids = room_ids[:i] + [room.id] + room_ids[i:5] #shifts right together, while keeping track of ids
-        # if max[0] == 2: #since private messages are being counted, this might be a case
-        #     chatRoom = "None"
-        #     return jsonify({"data: chatRoom"})
-        # chatRoom = [] #to hold the 6 most popular rooms
-        # for room_id in room_ids:
-        #     for room in ChatRoomModel.Objects.get_or_404(id=room_id):
-        #         chatRoom.push(room)
+        # check and decide #mk #tb
+        max = [-1,-1,-1,-1,-1,-1]
+        room_ids = [-1,-1,-1,-1,-1,-1]
+        for room in ChatRoomModel.objects.get_or_404(): #(privateMessaging=False) might help
+            for i in range(max.length):
+                if room.members.length > max[i]:
+                    max = max[:i] + [room.members.length] + max[i:5] #shift right
+                    room_ids = room_ids[:i] + [room.id] + room_ids[i:5] #shifts right together, while keeping track of ids
+        if max[0] == 2: #since private messages are being counted, this might be a case
+            chatRoom = "None"
+            return jsonify({"data: chatRoom"})
+        chatRoom = [] #to hold the 6 most popular rooms
+        for room_id in room_ids:
+            for room in ChatRoomModel.Objects.get_or_404(id=room_id):
+                chatRoom.push(room)
 
         return jsonify({"data": chatRoom})
